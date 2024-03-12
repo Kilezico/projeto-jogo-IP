5c577ddf5d8b68c766a528ab62549353e60bc014
Henrique <henriquepassos2004@outlook.com.br>
2024-03-12 18:30:45 -0300

    Deixei a movimentação do personagem mais rápida

--- include/phisica.h
+++ include/phisica.h
@@ -3,6 +3,6 @@
 
 #define G 1000 // Gravidade
 #define PLAYER_JUMP_SPD 700.0f // Velocidade do pulo vertical
-#define PLAYER_HOR_SPD 200.0f // Velocidade horizontal
+#define PLAYER_HOR_SPD 300.0f // Velocidade horizontal
 
 #endif
