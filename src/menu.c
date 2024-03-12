1c433705ee8aba6e82b4f61603926cbb89dde290
claramtenorio <158226328+claramtenorio@users.noreply.github.com>
2024-03-11 22:26:28 -0300

    Update menu.c

--- src/menu.c
+++ src/menu.c
@@ -3,8 +3,8 @@
 #include <math.h>
 #include <stdlib.h>
 
-#define BUTTON_WIDTH 200 //tamanho dos botões (horizontal) 
-#define BUTTON_HEIGHT 50 //tamanho dos botões (vertical) 
+#define BUTTON_WIDTH 450 //define a largura do botões
+#define BUTTON_HEIGHT 110 //define a altura dos botões 
 
 typedef struct {
     Rectangle button;
@@ -17,7 +17,7 @@ typedef struct {
 void DrawMenuButton(options choice) {
 
     DrawRectangleRec(choice.button, choice.rectColor);
-    DrawText(choice.word, (int)choice.button.x + 10, (int)choice.button.y + 10, choice.fontSize, choice.optionColor);
+    DrawText(choice.word, (int)choice.button.x + 20, (int)choice.button.y + 20, choice.fontSize, choice.optionColor);
 } //desenha cada um dos botões com base nas informações da struct correspondente a opção (parametro)
 
 bool checkOptionSelected(bool buttonClicked, options choice) {
@@ -32,10 +32,10 @@ bool checkOptionSelected(bool buttonClicked, options choice) {
     return buttonClicked;
 } //checa se o botão fornecido no parametro foi clicado, caso sim torna buttonClicked verdade
 
-void menu()
+int menu(void)
 {
-    const int screenWidth = 800; //tamanho da tela na horizontal
-    const int screenHeight = 450; //tamanho da tela na vertical 
+    const int screenWidth = 1920; //tamanho da tela na horizontal
+    const int screenHeight = 1080; //tamanho da tela na vertical 
     Color forestGreen = (Color){34, 139, 34, 255}; //cor para os nomes das opções
  
     InitWindow(screenWidth, screenHeight, "sapo-sopa sobe menu screen");
@@ -46,36 +46,36 @@ void menu()
     options play; options gamePlay; options backStory; options exit; options creditPage;
     //struct que pega as informações dos botões
 
-    play.button = (Rectangle){ screenWidth - (80 + BUTTON_WIDTH), screenHeight / 2 - (5*BUTTON_HEIGHT / 2), BUTTON_WIDTH, BUTTON_HEIGHT };
+    play.button = (Rectangle){ screenWidth - (470 + BUTTON_WIDTH), screenHeight / 2 - (5*BUTTON_HEIGHT / 2), BUTTON_WIDTH, BUTTON_HEIGHT };
     //define o retangulo que representa o botão, todos tem o mesmo tamanho mas a coordenada do eixo y muda, um em cima do outro
     play.rectColor = SKYBLUE; //cor do retengulo, igual a do fundo e igual para todos
     play.optionColor = forestGreen; //cor do texto, igual para todos
     play.word = "Jogar"; //texto q fica no botão, indica a opção
-    play.fontSize = 30; //tamanho das letras
+    play.fontSize = 65; //tamanho das letras
 
-    gamePlay.button = (Rectangle){ screenWidth - (80 + BUTTON_WIDTH), 10 + screenHeight / 2 - (3*BUTTON_HEIGHT / 2), BUTTON_WIDTH, BUTTON_HEIGHT };
+    gamePlay.button = (Rectangle){ screenWidth - (470 + BUTTON_WIDTH), 25 + screenHeight / 2 - (3*BUTTON_HEIGHT / 2), BUTTON_WIDTH, BUTTON_HEIGHT };
     gamePlay.rectColor = SKYBLUE;
     gamePlay.optionColor = forestGreen;
     gamePlay.word = "Como jogar?";
-    gamePlay.fontSize = 30;
+    gamePlay.fontSize = 65;
 
-    backStory.button = (Rectangle){ screenWidth - (80 + BUTTON_WIDTH), 20 + screenHeight / 2 - BUTTON_HEIGHT / 2, BUTTON_WIDTH, BUTTON_HEIGHT };
+    backStory.button = (Rectangle){ screenWidth - (470 + BUTTON_WIDTH), 50 + screenHeight / 2 - BUTTON_HEIGHT / 2, BUTTON_WIDTH, BUTTON_HEIGHT };
     backStory.rectColor = SKYBLUE;
     backStory.optionColor = forestGreen;
     backStory.word = "Backstory";
-    backStory.fontSize = 30;
+    backStory.fontSize = 65;
 
-    exit.button = (Rectangle){ screenWidth - (80 + BUTTON_WIDTH), 30 + screenHeight / 2 + BUTTON_HEIGHT / 2, BUTTON_WIDTH, BUTTON_HEIGHT };
+    exit.button = (Rectangle){ screenWidth - (470 + BUTTON_WIDTH), 75 + screenHeight / 2 + BUTTON_HEIGHT / 2, BUTTON_WIDTH, BUTTON_HEIGHT };
     exit.rectColor = SKYBLUE;
     exit.optionColor = forestGreen;
     exit.word = "Sair :(";
-    exit.fontSize = 30;
+    exit.fontSize = 65;
 
-    creditPage.button = (Rectangle){ screenWidth - (80 + BUTTON_WIDTH), 40 + screenHeight / 2 + 3*BUTTON_HEIGHT / 2, BUTTON_WIDTH, BUTTON_HEIGHT };
+    creditPage.button = (Rectangle){ screenWidth - (470 + BUTTON_WIDTH), 100 + screenHeight / 2 + 3*BUTTON_HEIGHT / 2, BUTTON_WIDTH, BUTTON_HEIGHT };
     creditPage.rectColor = SKYBLUE;
     creditPage.optionColor = forestGreen;
     creditPage.word = "Creditos";
-    creditPage.fontSize = 30;
+    creditPage.fontSize = 65;
 
     bool buttonPlayClicked = false, buttonGamePlayClicked = false, buttonBackStoryClicked = false, buttonExitClicked = false, buttonCreditPageClicked = false;
     //variavel booleana pra verificar se alguma opção foi selecionada, so muda e fica verdadeira caso seja, quando se torna true a tela muda
@@ -86,13 +86,13 @@ void menu()
         BeginDrawing();
 
             ClearBackground(SKYBLUE);
-            DrawText("sapo-sopa sobe", 115, 15, 70, forestGreen); //titulo do jogo
+            DrawText("sapo-sopa sobe", 275, 35, 170, forestGreen); //titulo do jogo
             
             Rectangle proportionFrogMenu = { 0.0f, 0.0f, frogMenu.width, frogMenu.height };
             //retangulo que vai definir a proporção da imagem do sapo, nesse caso pega o sapo todo
             // Define the destination rectangle for drawing
-            Rectangle whereFrogMenu = { 265, 275, frogMenu.width * 0.25, frogMenu.height * 0.25 };
-            //define onde deve ser desenhado e o seu tamanho (escala de 0.25)
+            Rectangle whereFrogMenu = { 635, 660, frogMenu.width * 0.5, frogMenu.height * 0.5 };
+            //define onde deve ser desenhado e o seu tamanho (escala de 0.6)
             Vector2 origin = { whereFrogMenu.width / 2, whereFrogMenu.height / 2 };
             //define o centro da imagem como sua origem
             DrawTexturePro(frogMenu, proportionFrogMenu, whereFrogMenu, origin, 0.0f, WHITE);
@@ -119,4 +119,6 @@ void menu()
 
     UnloadTexture(frogMenu);
     CloseWindow(); 
+
+    return 0;
 }
