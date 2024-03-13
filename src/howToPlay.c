ed18ad925ce3372ca0bc87e6f94dc498ecf1ea48
claramtenorio <158226328+claramtenorio@users.noreply.github.com>
2024-03-13 16:04:56 -0300

    Update howToPlay.c

--- src/howToPlay.c
+++ src/howToPlay.c
@@ -23,34 +23,42 @@ GameScreen howToDraw(GameScreen *screen)
     Texture2D LandR = LoadTexture("assets/LandR.png");
     Texture2D spaceKey = LoadTexture("assets/spaceKey.png");
 
-    while (!WindowShouldClose())
+    while (goBacktoMenu!=true)
     {
 
         BeginDrawing();
 
-        DrawTexture(background, 0, 0, WHITE);
+            DrawTexture(background, 0, 0, WHITE);
+    
+            Rectangle backToMenu = (Rectangle){ 20, 30, 450, 200 };
+            DrawRectangleRec(backToMenu, polutedSky);
+            DrawText("sapo-sopa sobe", 25, 35, 50, BLACK);
+            goBacktoMenu = BackToMenuPressedC(goBacktoMenu, backToMenu);
+            if(goBacktoMenu==true){//vai fazer o while parar
+                *screen = MENUS;
+            }
       
-        Rectangle keyBoardProportions = { 0, 0, keyBoard.width, keyBoard.height };
-        Rectangle keyBoardWhere = { 1440, 480, keyBoard.width*0.5, keyBoard.height*0.5 };
-        DrawTexturePro(keyBoard, keyBoardProportions, keyBoardWhere, (Vector2){ 0, 0 }, 0, WHITE); //coloca a imagem do teclado
-
-        Rectangle AandDProportions = { 0, 0, AandD.width, AandD.height };
-        Rectangle AandDWhere = { 120, 1200, AandD.width*0.25, AandD.height*0.25 };
-        DrawTexturePro(AandD, AandDProportions, AandDWhere, (Vector2){ 0, 0 }, 0, WHITE); //imagem das teclas a e d
-
-        Rectangle LandRProportions = { 0, 0, LandR.width, LandR.height };
-        Rectangle LandRWhere = { 140, 1200, LandR.width*0.25, LandR.height*0.25 };
-        DrawTexturePro(LandR, LandRProportions, LandRWhere, (Vector2){ 0, 0 }, 0, WHITE); //imagem das teclas das setas
-
-        Rectangle spaceKeyProportions = { 0, 0, spaceKey.width, spaceKey.height };
-        Rectangle spaceKeyWhere = { 300, 1200, spaceKey.width*0.25, spaceKey.height*0.25 };
-        DrawTexturePro(spaceKey, spaceKeyProportions, spaceKeyWhere, (Vector2){ 0, 0 }, 0, WHITE); //imagem da tecla espaço
-
-        DrawText("Teclas necessárias:", 100, 70, 100, BLACK);
-        DrawText("Como andar para os lados?", 120, 200, 50, BLACK);
-        DrawText("Use as letras A e D, ou as setas do teclado", 120, 250, 25, BLACK);
-        DrawText("Como pular?", 960, 480, 50, BLACK);
-        DrawText("Aperte no botão de espaço", 960, 600, 25, BLACK); //textos que ficam na tela
+            Rectangle keyBoardProportions = { 0, 0, keyBoard.width, keyBoard.height };
+            Rectangle keyBoardWhere = { 1440, 480, keyBoard.width*0.5, keyBoard.height*0.5 };
+            DrawTexturePro(keyBoard, keyBoardProportions, keyBoardWhere, (Vector2){ 0, 0 }, 0, WHITE); //coloca a imagem do teclado
+    
+            Rectangle AandDProportions = { 0, 0, AandD.width, AandD.height };
+            Rectangle AandDWhere = { 120, 1200, AandD.width*0.25, AandD.height*0.25 };
+            DrawTexturePro(AandD, AandDProportions, AandDWhere, (Vector2){ 0, 0 }, 0, WHITE); //imagem das teclas a e d
+    
+            Rectangle LandRProportions = { 0, 0, LandR.width, LandR.height };
+            Rectangle LandRWhere = { 140, 1200, LandR.width*0.25, LandR.height*0.25 };
+            DrawTexturePro(LandR, LandRProportions, LandRWhere, (Vector2){ 0, 0 }, 0, WHITE); //imagem das teclas das setas
+    
+            Rectangle spaceKeyProportions = { 0, 0, spaceKey.width, spaceKey.height };
+            Rectangle spaceKeyWhere = { 300, 1200, spaceKey.width*0.25, spaceKey.height*0.25 };
+            DrawTexturePro(spaceKey, spaceKeyProportions, spaceKeyWhere, (Vector2){ 0, 0 }, 0, WHITE); //imagem da tecla espaço
+    
+            DrawText("Teclas necessárias:", 100, 70, 100, BLACK);
+            DrawText("Como andar para os lados?", 120, 200, 50, BLACK);
+            DrawText("Use as letras A e D, ou as setas do teclado", 120, 250, 25, BLACK);
+            DrawText("Como pular?", 960, 480, 50, BLACK);
+            DrawText("Aperte no botão de espaço", 960, 600, 25, BLACK); //textos que ficam na tela
 
         EndDrawing();
     }
@@ -61,5 +69,5 @@ GameScreen howToDraw(GameScreen *screen)
     UnloadTexture(LandR);
     UnloadTexture(spaceKey);
 
-    return 0;
+    return screen;
 }
