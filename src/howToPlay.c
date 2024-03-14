05d2f22b09dafae1667f7c3adbc75ad750f4d764
claramtenorio <158226328+claramtenorio@users.noreply.github.com>
2024-03-13 23:48:18 -0300

    Update howToPlay.c

--- src/howToPlay.c
+++ src/howToPlay.c
@@ -36,10 +36,10 @@ GameScreen howToDraw(GameScreen *screen, Font fonte)
                 *screen = MENUS;
             }
     
-            DrawTextEx(fonte, "Como pular?", (Vector2){120, 500}, 200, 0, BLACK);
-            DrawTextEx(fonte, "Aperte no botão de espaço", (Vector2){120, 670}, 100, 0, BLACK);
-            DrawTextEx(fonte, "Como andar para os lados?", (Vector2){960, 780}, 200, 0, BLACK);
-            DrawTextEx(fonte, "Use as letras A e D,\n\nou as setas do teclado", (Vector2){1100, 950}, 100, 0, BLACK); //textos que ficam na tela
+            DrawTextEx(fonte, "Como pular?", (Vector2){120, 450}, 200, 0, BLACK);
+            DrawTextEx(fonte, "Aperte no botão de espaço", (Vector2){120, 620}, 100, 0, BLACK);
+            DrawTextEx(fonte, "Como andar para os lados?", (Vector2){960, 710}, 200, 0, BLACK);
+            DrawTextEx(fonte, "Use as letras A e D,\n\n\nou as setas do teclado", (Vector2){1100, 880}, 100, 0, BLACK); //textos que ficam na tela
             
         EndDrawing();
     }
