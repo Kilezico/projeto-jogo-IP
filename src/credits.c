22917935ea5fc47f18b1b4b9556d8efd7e390bd2
claramtenorio <158226328+claramtenorio@users.noreply.github.com>
2024-03-13 23:00:31 -0300

    Update credits.c

--- src/credits.c
+++ src/credits.c
@@ -114,7 +114,7 @@ GameScreen creditsDraw(GameScreen *screen, Font fonte)
             Rectangle backToMenu = (Rectangle){ 20, 30, 450, 200 };
             DrawRectangleRec(backToMenu, polutedSky);
             // DrawText("sapo-sopa sobe", 25, 35, 50, BLACK);
-            DrawTextEx(fonte, "sapo-sopa sobe", (Vector2){25, 35}, 50, 0, BLACK);
+            DrawTextEx(fonte, "menu", (Vector2){25, 35}, 50, 0, BLACK);
             goBacktoMenu = BackToMenuPressedC(goBacktoMenu, backToMenu);
             if(goBacktoMenu==true){//vai fazer o while parar
                 *screen = MENUS;
